99190e76b4391ad0e1ef2eb38e94643435c8b3f8
Paul Kastel <pkastelik@poczta.fm>
2017-05-20 14:16:25 +0200

    Klasyka gatunku - ciag fibonacciego policzony na 2 sposoby

---
+++ Fibonacci.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+/*
+Dwa sposoby na policzenie n-tego wyrazu ci¹gu fibonacciego
+*/
+
+
+/// <summary>
+/// Fibonacci policzony rekurencyjnie
+/// </summary>
+/// <param name="num">n-ty element ciagu</param>
+/// <returns>wartosc n-tego elementu w ciagu</returns>
+int fibonacci_rec(int num)
+{
+	if (num == 0 || num == 1)
+		return num;
+	else
+		return fibonacci_rec(num - 1) + fibonacci_rec(num - 2);
+}
+
+/// <summary>
+/// Fibonacci policzony bez uzycia rekurencji
+/// </summary>
+/// <param name="num">n-ty element ciagu</param>
+/// <returns>wartosc n-tego elementu w ciagu</returns>
+int fibonacci_norm(int num)
+{
+	int next = 1, prev = 1, curr=0;
+	for (int i = 2; i < num; i++)
+	{
+		curr = next + prev;
+		next = prev;
+		prev = curr;
+	}
+	return curr;
+}
+
+int main(int argc, char *argv[])
+{
+
+	int count=0;
+	int input=0;
+	printf("Wprowadz numer ciagu (mniej niz 48): ");
+	scanf_s("%d", &input);
+
+	count = fibonacci_rec(input);
+	printf("Wynik uzyskany rekurencyjnie: %d\n", count);
+	count = fibonacci_norm(input);
+	printf("Wynik uzyskany normalnie: %d\n", count);
+
+	getchar();
+	getchar();
+	return 0;
+}
